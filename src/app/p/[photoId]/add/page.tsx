7def3d2906250e63d5593f55060610ab70e549ed
Xavier MALPARTY <xavier@nimblehq.co>
2024-01-13 21:23:17 +0700

    Fix build

--- src/app/p/[photoId]/add/page.tsx
+++ src/app/p/[photoId]/add/page.tsx
@@ -1,16 +1,14 @@
 import { getPhotoCached } from '@/cache';
-import AddModal from '@/checkout/AddModal';
+import PhotoAddModal from '@/photo/PhotoAddModal';
 import { PATH_ROOT } from '@/site/paths';
 import { redirect } from 'next/navigation';
 
-export default async function Share({
-  params: { photoId },
-}: {
-  params: { photoId: string }
-}) {
+export default async function Share({ params: { photoId } }: { params: { photoId: string } }) {
   const photo = await getPhotoCached(photoId);
 
-  if (!photo) { return redirect(PATH_ROOT); }
+  if (!photo) {
+    return redirect(PATH_ROOT);
+  }
 
-  return <AddModal photo={photo} />;
+  return <PhotoAddModal photo={photo} />;
 }
--- src/photo/PhotoAddModal.tsx
+++ src/photo/PhotoAddModal.tsx
@@ -1,5 +1,5 @@
 import PhotoOGTile from '@/photo/PhotoOGTile';
-import { absolutePathForPhoto, pathForPhoto } from '@/site/paths';
+import { pathForPhoto } from '@/site/paths';
 import { Photo } from '.';
 import AddModal from '@/checkout/AddModal';
 import { Camera } from '@/camera';
