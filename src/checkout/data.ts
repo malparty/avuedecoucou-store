6653330b133508ff8775b9994886d66cf2afdd0b
Xavier MALPARTY <xavier@nimblehq.co>
2024-02-24 13:09:59 +0700

    Update 2024 prices

--- src/checkout/data.ts
+++ src/checkout/data.ts
@@ -8,8 +8,8 @@ type formatType = keyof formatsType;
 export const FORMATS: formatsType = { a: '45x30mm', b: '60x40mm', c: '75x50mm', d: '90x60mm', e: '105x70mm' };
 export const SUPPORTS: supportType[] = ['toile', 'forex', 'aluminium', 'acrylique'];
 export const PRICES: {[key in supportType]: { [key in formatType]: number }} = {
-  'toile': { a: 135, b: 140, c: 150, d: 180, e: 240 },
-  'forex': { a: 160, b: 165, c: 175, d: 190, e: 260 },
-  'aluminium': { a: 200, b: 210, c: 240, d: 270, e: 285 },
-  'acrylique': { a: 210, b: 220, c: 250, d: 280, e: 300 },
+  'toile': { a: 140, b: 150, c: 160, d: 190, e: 270 },
+  'forex': { a: 170, b: 180, c: 190, d: 200, e: 280 },
+  'aluminium': { a: 210, b: 220, c: 240, d: 280, e: 300 },
+  'acrylique': { a: 220, b: 230, c: 260, d: 290, e: 320 },
 };
